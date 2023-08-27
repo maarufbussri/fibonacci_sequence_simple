a4543033083cb1428ae35c162ead4791acd54d92
Maaruf Bussri <134280908+maarufbussri@users.noreply.github.com>
2023-08-27 22:13:29 +0800

    fibonacci_sequence_simple.c

---
+++ fibonacci_sequence_simple.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0, j = 1, k, l = 1;
+
+	printf("0 1 ");
+
+	do
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		l = l++;
+		printf("%d ", k);
+	}
+	while (l < 10);
+
+	return 0;
+}
